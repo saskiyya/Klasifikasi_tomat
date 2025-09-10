f404539f998f302bc9a962e61c695b6a90c22305
saskiyya <saskiahumaira02@gmail.com>
2025-09-10 11:30:47 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-impot pandas as pd
+import pandas as pd
 import joblib
 
 st.set_page_config(
@@ -10,16 +10,17 @@ st.set_page_config(
 model = joblib.load("model_klasifikasi_tomat.joblib")
 scaler = joblib.load("scaler_klasifikasi_tomat.joblib")
 
-st.title("st.title("🍅 Klasifikasi Tomat")
-st.markdown("Aplikasi machine learning untuk klasifikasi apakah tomat termasuk kategori **Ekspor, Lokal premium atau Industri**")
+st.title("🍅 Klasifikasi Tomat")
+st.markdown("Aplikasi machine learning untuk klasifikasi tomat termasuk kategori **Ekspor, Lokal Premium, atau Industri**")
 
-berat = st.slider("Berat Tomat", 50, 200, 85)
-kekenyalan = s.slider("Tingkat kekenyalan", 3.0, 10.0, 5.0)
-kadar_gula = st.slider("Kadar Gula", 2.0, 10.0, 8.0)
+berat = st.slider("Berat Tomat", 50, 200, 80)
+kekenyalan = st.slider("Tingkat Kekenyalan", 2.0, 10.0, 4.2)
+kadar_gula = st.slider("Kadar Gula", 1.0, 10.0, 5.3)
 tebal_kulit = st.slider("Tebal Kulit", 0.1, 1.0, 0.7)
 
-if st.button("Prediksi"):
-	data = pd,DataFrame([[berat, kekenyalan, kadar_gula, tebal_kulit]]	columns=["berat","kekenyalan","kadar_gula","tebal_kulit"])
+if st.button("Prediksi", type="primary"):
+	data_baru = pd.DataFrame([[berat,kekenyalan,kadar_gula,tebal_kulit]],
+				columns=["berat","kekenyalan","kadar_gula","tebal_kulit"])
 
 	data_baru_scaled = scaler.transform(data_baru)
 	prediksi = model.predict(data_baru_scaled)[0]
@@ -29,4 +30,5 @@ if st.button("Prediksi"):
 
 st.divider()
 st.caption("Dibuat dengan 🍅 oleh **saskyaa**")
+
 
